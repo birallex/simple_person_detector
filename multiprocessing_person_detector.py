1c75bf23fd6771bbeb0a20815ee2bc7f6baab200
birallex <biralex204@gmail.com>
2021-02-22 09:16:43 +0300

    add new script that allows detect people's through the multiple cameras in same cameras

---
+++ multiprocessing_person_detector.py
@@ -0,0 +1,68 @@
+from imutils.video import VideoStream
+from multiprocessing import Process
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+
+def stream_detector(stream_number):
+	vs = VideoStream(src=stream_number).start()
+	time.sleep(2.0)
+	while True:
+		frame = vs.read()
+		frame = imutils.resize(frame, width=400)
+		(h, w) = frame.shape[:2]
+		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+			0.007843, (300, 300), 127.5)
+		net.setInput(blob)
+		detections = net.forward()
+		for i in np.arange(0, detections.shape[2]):
+			confidence = detections[0, 0, i, 2]
+
+			if confidence > args["confidence"]:
+				idx = int(detections[0, 0, i, 1])
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
+				if CLASSES[idx] == "person":
+					label = "{}: {:.2f}%".format(CLASSES[idx],
+						confidence * 100)
+					cv2.rectangle(frame, (startX, startY), (endX, endY),
+						COLORS[idx], 2)
+					y = startY - 15 if startY - 15 > 15 else startY + 15
+					cv2.putText(frame, label, (startX, y),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)    
+		#frame = cv2.resize(frame, (768, 768))
+
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("q"):
+			break
+	cv2.destroyAllWindows()
+	vs.stop()
+
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-c", "--confidence", type=float, default=0.6,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+net = cv2.dnn.readNetFromCaffe("weights/MobileNetSSD_deploy.prototxt.txt",  "weights/MobileNetSSD_deploy.caffemodel") 
+
+if __name__ == "__main__":
+	procs = []
+	proc_1 = Process(target=stream_detector, args=(0,))
+	proc_2 = Process(target=stream_detector, args=(2,))
+	procs.append(proc_1)
+	procs.append(proc_2)
+	proc_1.start()
+	proc_2.start()
